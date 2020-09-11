d09772f437bb622ea94bafa8fbb4a92a8fc0218b
Alex Putnam <a.kk.putnam@gmail.com>
2020-09-11 12:42:54 -0700

    trying to get buttons to function

--- src/App.js
+++ src/App.js
@@ -3,14 +3,30 @@ import List from "./List";
 import STORE from "./STORE";
 import "./App.css";
 
+function omit(obj, keyToOmit) {
+  let { [keyToOmit]: _, ...rest } = obj;
+  return rest;
+}
+
 class App extends React.Component {
   state = {
     store: STORE,
   };
 
-  handleDeleteCard(id) {
-    console.log("handle deleted card called", { id });
-  }
+  handleDeleteCard = (cardId) => {
+    console.log("handle deleted card called", { cardId });
+    const newLists = this.state.store.lists.map((list) => ({
+      ...list,
+      cardIds: list.cardIds.filter((id) => id !== cardId),
+    }));
+    const newCards = omit(this.state.store, cardId);
+    this.setState({
+      store: {
+        lists: newLists,
+        allCards: newCards,
+      },
+    });
+  };
 
   handleAddRandomCard() {
     console.log("handle add random card called");
@@ -26,6 +42,7 @@ class App extends React.Component {
           {this.state.store.lists.map((list) => (
             <List
               key={list.id}
+              id={list.id}
               header={list.header}
               cards={list.cardIds.map((id) => this.state.store.allCards[id])}
               onDeleteCard={this.handleDeleteCard}
